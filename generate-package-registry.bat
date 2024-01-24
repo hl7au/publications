cf73f04db1ca619504815b463ebbf78de5e5d6ae
Brett Esler <brett.esler@teamunicorn.net>
2024-01-25 01:52:51 +1100

    sample build script

--- generate-package-registry.bat
+++
@@ -1,1 +0,0 @@
-java -jar publisher.jar -generate-package-registry %CD%\webroot  
--- go-publish.bat
+++ go-publish.bat
@@ -3,5 +3,18 @@ git clone https://github.com/HL7/fhir-ig-history-template.git ig-history
 git clone https://github.com/hl7au/ig-registry.git ig-registry
 
 git clone https://github.com/hl7au/au-fhir-core %CD%\hl7au\au-fhir-core
+ren CD%\hl7au\au-fhir-core\package-list.json CD%\hl7au\au-fhir-core\package-list.json.000
+
+java -jar publisher.jar -generate-package-registry %CD%\webroot
+
+curl.exe --output %CD%\webroot\fhir\core\package-list.json --url https://hl7.org.au/fhir/core/package-list.json
+curl.exe --output %CD%\webroot\fhir\package-feed.xml --url https://hl7.org.au/fhir/package-feed.xml
+curl.exe --output %CD%\webroot\fhir\publication-feed.xml --url https://hl7.org.au/fhir/publication-feed.xml
+
+
+cd %CD%\hl7au\au-fhir-core
+_updatePublisher.bat
+_genonce.bat
+cd ..\..
 
 java -jar publisher.jar -go-publish -source %CD%\hl7au\au-fhir-core -web %CD%\webroot -history %CD%\ig-history -registry %CD%\ig-registry\fhir-ig-list.json -temp %CD%\temp -templates %CD%\templates
