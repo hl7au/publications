7c8cad5f9f6302b33d8ce3a763a83f1048db1096
Brett Esler <brett.esler@teamunicorn.net>
2024-03-01 14:21:26 +1100

    added AU Base steps to batch

--- go-publish.bat
+++ go-publish.bat
@@ -5,9 +5,16 @@ git clone https://github.com/hl7au/ig-registry.git ig-registry
 git clone https://github.com/hl7au/au-fhir-core %CD%\hl7au\au-fhir-core
 del %CD%\hl7au\au-fhir-core\package-list.json
 
+git clone https://github.com/hl7au/au-fhir-base %CD%\hl7au\au-fhir-base
+del %CD%\hl7au\au-fhir-base\package-list.json
+
 mkdir %CD%\webroot\fhir
 mkdir %CD%\webroot\fhir\core
 curl.exe --output %CD%\webroot\fhir\core\package-list.json --url https://hl7.org.au/fhir/core/package-list.json
+
+mkdir %CD%\webroot\fhir\base
+curl.exe --output %CD%\webroot\fhir\base\package-list.json --url https://hl7.org.au/fhir/package-list.json
+
 curl.exe --output %CD%\webroot\fhir\package-feed.xml --url https://hl7.org.au/fhir/package-feed.xml
 curl.exe --output %CD%\webroot\fhir\publication-feed.xml --url https://hl7.org.au/fhir/publication-feed.xml
 
@@ -18,4 +25,11 @@ call _updatePublisher.bat
 call _genonce.bat
 cd ..\..
 
+cd %CD%\hl7au\au-fhir-base
+call _updatePublisher.bat
+call _genonce.bat
+cd ..\..
+
 java -jar publisher.jar -go-publish -source %CD%\hl7au\au-fhir-core -web %CD%\webroot -history %CD%\ig-history -registry %CD%\ig-registry\fhir-ig-list.json -temp %CD%\temp -templates %CD%\templates
+
+java -jar publisher.jar -go-publish -source %CD%\hl7au\au-fhir-base -web %CD%\webroot -history %CD%\ig-history -registry %CD%\ig-registry\fhir-ig-list.json -temp %CD%\temp -templates %CD%\templates
